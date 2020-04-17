24e460d049ee1d86c87162c266d7cf36c2135f94
ks <834462737@qq.com>
2020-04-17 20:42:20 +0800

    1

---
+++ 单签解析成目录/ipa.php
@@ -0,0 +1,16 @@
+<?php
+$path = __DIR__ . '/ipas';
+$ipas = scandir($path);
+foreach ($ipas as $ipa) {
+    if ($ipa == '..' || $ipa == '.') {
+        continue;
+    }
+    preg_match('/(.*)_.*\.ipa/', $ipa, $result);
+    $pinyin = $result[1];
+    $filePath = __DIR__ . '/games/' . $pinyin . '/ios/' . str_replace('.ipa','-dyt.ipa', $ipa);
+    if (!is_dir(dirname($filePath))) {
+        mkdir(dirname($filePath), 0777, true);
+    }
+    copy($path . '/' . $ipa, $filePath);
+    print_r(PHP_EOL);
+}
